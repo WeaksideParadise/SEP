c1fc79c5fff06ede1e2c1437a402b0937a44c24c
Markus Meier <Weaksideparadise@gmail.com>
2024-09-03 17:26:46 +0200

    test main added

--- Meilenstein_2/test_main.py
+++ Meilenstein_2/test_main.py
@@ -2,4 +2,10 @@ from Tests import test_Database, test_Ressource, test_RessourceActions, test_Res
 import unittest
 
 if __name__ == '__main__':
-    test_Database.test_Database("test_connect_failure")
+    test = test_User.test_User()
+    test.setUp()
+   # test.test_invalid_user_id()
+    test.test_getters()
+    test.test_invalid_hashed_password()
+    test.test_setters()
+    test.test_name_length()
